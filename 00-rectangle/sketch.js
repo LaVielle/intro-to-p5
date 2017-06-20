54a8fde34eaeb669a8f8cfe10677bb90bcdddd69
LaVielle <Sam@Samuels-MBP.local.tld>
2017-06-20 11:45:34 -0400

    rect example

---
+++ 00-rectangle/sketch.js
@@ -0,0 +1,19 @@
+function setup(){
+   //create the canvas in which you will draw stuff
+   createCanvas(500,500)
+
+   // set the background color to black
+   background(0)
+
+   // set the rectangle's color to white
+   fill(255)
+
+   // draw a rectangle to the canvas
+   rect(100, 100, 50, 100)
+}
+
+
+// we are not using the draw() function yet...
+function draw(){
+
+}
